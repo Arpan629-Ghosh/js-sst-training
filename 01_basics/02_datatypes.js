e397daec6560f1d4fe59fb5b9399675ffcd6726d
Arpan <arpanthe123@gmail.com>
2026-02-14 12:49:44 +0530

    datatype summary

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -14,3 +14,28 @@ let isLoggedIn = true;
 
 console.log(typeof null);
 console.log(typeof undefined);
+
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100;
+const id = Symbol("123");
+const anotherId = Symbol("123");
+console.log(id === anotherId);
+const bigNumber = 34239844538823110n;
+// Non Primitive (Reference type)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+  name: "arpan",
+  age: 21,
+};
+
+const myFunction = function () {
+  console.log("Arpan Ghosh");
+};
+
+console.log(typeof bigNumber);
