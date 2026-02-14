23dc5da53d605fd844d0762bc208833f24487222
Arpan <arpanthe123@gmail.com>
2026-02-14 11:15:47 +0530

    datatypes 1

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "Arpan";
+let age = 21;
+let isLoggedIn = true;
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned any value
+// symbol => unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
